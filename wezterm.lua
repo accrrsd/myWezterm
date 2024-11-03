f97e8d5cfb9b08c8f794b4f6ec0d73938ce85330
Daniil <kashel369@bk.ru>
2024-11-03 16:09:36 +0300

    second iteration

--- wezterm.lua
+++ wezterm.lua
@@ -8,7 +8,18 @@ c.window_background_opacity = 0.9
 c.enable_tab_bar = false
 c.window_decorations="RESIZE"
 c.max_fps = 144
+c.automatically_reload_config = true
+c.window_padding = {
+  left = 0,
+  right = 0,
+  top = 0,
+  bottom = 0,
+}
+c.enable_scroll_bar = false
 
+if wezterm.target_triple == "x86_64-pc-windows-msvc" then
+  c.default_prog = {"powershell.exe"}
+end
 
 c.keys = {
   -- paste from the clipboard
