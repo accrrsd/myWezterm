84eda4378b0640599e66d3a860246258881776df
Daniil <kashel369@bk.ru>
2025-03-03 04:05:40 +0300

    command

--- wezterm.lua
+++ wezterm.lua
@@ -1,32 +1,47 @@
-local wezterm = require 'wezterm';
+local wezterm = require("wezterm")
 local act = wezterm.action
 local c = wezterm.config_builder()
 
-c.font = wezterm.font("Fira Code")
+c.font = wezterm.font("FiraCode Nerd Font")
 c.font_size = 12.0
 c.window_background_opacity = 0.9
 c.enable_tab_bar = false
-c.window_decorations="RESIZE"
+
+-- Функция для изменения размера шрифта
+local function set_font_size(window, _pane, new_size)
+	window:set_config_overrides({ font_size = new_size })
+end
+
+-- Обработчик события изменения пользовательской переменной
+wezterm.on("user-var-changed", function(window, pane, name, value)
+	if name == "WEZTERM_FONT_SIZE" then
+		local new_size = tonumber(value)
+		if new_size then
+			set_font_size(window, pane, new_size)
+		end
+	end
+end)
+
+c.window_decorations = "RESIZE"
 c.max_fps = 144
 c.automatically_reload_config = true
 c.window_padding = {
-  left = 0,
-  right = 0,
-  top = 0,
-  bottom = 0,
+	left = 0,
+	right = 0,
+	top = 0,
+	bottom = 0,
 }
 c.enable_scroll_bar = false
 
 if wezterm.target_triple == "x86_64-pc-windows-msvc" then
-  c.default_prog = {"powershell.exe"}
+	c.default_prog = { "powershell.exe" }
 end
 
-c.keys = {
-  -- paste from the clipboard
-  { key = 'v', mods = 'CTRL', action = act.PasteFrom 'Clipboard' },
+c.window_close_confirmation = "NeverPrompt"
 
-  -- paste from the primary selection
-  { key = 'v', mods = 'CTRL', action = act.PasteFrom 'PrimarySelection' },
+c.keys = {
+	{ key = "v", mods = "CTRL", action = act.PasteFrom("Clipboard") },
+	{ key = "v", mods = "CTRL", action = act.PasteFrom("PrimarySelection") },
 }
 
 return c
